43dcdbd9c6ff3b7afcbadabdf07c4b3d8f16bbc4
Hans Felix <hansfelix50@gmail.com>
2020-05-19 23:46:20 -0500

    add main.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,85 +1,77 @@
-
-let development = process.env.NODE_ENV !== 'production'
+let development = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: 'spa',
+  mode: "spa",
   generate: {
-    dir: 'docs'
+    dir: "docs"
   },
   router: {
-    base: development ? '/' : '/personal-page/' 
+    base: development ? "/" : "/personal-page/"
   },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: process.env.npm_package_description || "" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap" }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    'normalize.css/normalize.css'
-  ],
+   ** Global CSS
+   */
+  css: ["normalize.css/normalize.css", "@/assets/css/main.css"],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     // '@nuxtjs/eslint-module'
   ],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
+    "@nuxtjs/dotenv",
     // Doc: https://github.com/nuxt-community/style-resources-module
-    '@nuxtjs/style-resources'
+    "@nuxtjs/style-resources"
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   },
   /*
-  ** Sass vairables
-  */
+   ** Sass vairables
+   */
   styleResources: {
-    scss: [
-      "@/assets/scss/_mq.scss",
-      "@/assets/scss/_mixins.scss"
-    ]
+    scss: ["@/assets/scss/_mq.scss", "@/assets/scss/_mixins.scss"]
   },
 
   vue: {
@@ -88,4 +80,4 @@ module.exports = {
       devtools: true
     }
   }
-}
+};
