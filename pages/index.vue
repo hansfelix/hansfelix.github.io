4a3b8cd1c538e986fb65cb4c15df7f56e4cc3e44
Hans Felix <hansfelix50@gmail.com>
2020-05-21 17:48:33 -0500

    update index presentation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-    <p>Hola</p>
+    <div class="main-text">
+      <div class="main-text__overline">{{ $t("index.overline") }}</div>
+      <h1 class="main-text__title">Hans Felix</h1>
+      <div class="main-text__description">{{ $t("index.desciption") }}</div>
+    </div>
+    <div class="background">
+      <h-icon name="background" width="300" height="300"></h-icon>
+    </div>
+
     <div class="social">
       <a class="social__item" href="https://www.linkedin.com/in/hans-felix/" target="_blank">
         <h-icon name="linkedin" width="20" height="20"></h-icon>
@@ -26,6 +34,39 @@ export default {
 
 <style lang="scss">
 .container {
+  position: relative;
+  width: 100%;
+  max-width: 1600px;
+  padding: 10px 150px;
+  margin: 0px auto;
+}
+
+.main-text {
+  height: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &__overline {
+    color: $main-color;
+    font-family: "Inconsolata", monospace;
+    font-size: 20px;
+  }
+
+  &__title {
+    font-size: 75px;
+    font-weight: 500;
+    letter-spacing: -1px;
+    margin: 0 0 15px;
+  }
+
+  &__description {
+    max-width: 600px;
+    font-size: 18px;
+    line-height: 1.4;
+  }
 }
 
 .social {
@@ -48,12 +89,19 @@ export default {
     background: transparent;
     color: inherit;
     cursor: pointer;
-    transition: .2s background ease-in-out;
+    transition: 0.2s background ease-in-out;
     @include flex-and-center;
 
     &:hover {
       background-color: #0000001a;
     }
   }
+}
+
+.background {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  opacity: 0.08;
 }
 </style>
