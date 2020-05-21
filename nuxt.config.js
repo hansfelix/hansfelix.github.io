41d1b90815671512a0a70169b77aac026fba659f
Hans Felix <hansfelix50@gmail.com>
2020-05-21 17:48:21 -0500

    Add inconsolata font

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ module.exports = {
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap" }
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500&display=swap" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" }
     ]
   },
   /*
