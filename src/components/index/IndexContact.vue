07f64841b4885142c11555aa57d0c46057e4332e
Hans Felix <hansfelix50@gmail.com>
2020-06-03 22:33:06 -0500

    Fix grid on IndexContact

--- src/components/index/IndexContact.vue
+++ src/components/index/IndexContact.vue
@@ -1,12 +1,13 @@
 <template>
   <section class="index-contact">
-    <p class="index-contact__pre-title">Interested in working together?</p>
-    <h2 class="index-contact__title">Get in touch!</h2>
-    <p class="index-contact__text">
-      If you wanna get in touch, talk to me about a project collaboration or just say hi, send me an email to hansfelixramos@gmail.com I'll try my
-      best to get back to you! We can also be in contact through any of my social networks
-    </p>
-
+    <div class="index-contact__content">
+      <p class="index-contact__pre-title">Interested in <br> working together?</p>
+      <h2 class="index-contact__title">Get in touch!</h2>
+      <p class="index-contact__text">
+        If you wanna get in touch, talk to me about a project collaboration or just say hi, send me an email to hansfelixramos@gmail.com I'll try my
+        best to get back to you! We can also be in contact through any of my social networks
+      </p>
+    </div>
   </section>
 </template>
 
@@ -17,15 +18,19 @@ export default {};
 <style lang="scss">
 .index-contact {
   min-height: 100vh;
+  @include flex-and-center-vertical;
+
+  &__content {
+  }
 
   &__pre-title {
     color: #6495b5;
     font-family: "Inconsolata", monospace;
-    font-size: 20px;
+    font-size: 18px;
   }
   &__title {
     color: #6495b5;
-    font-size: 1.8em;
+    font-size: 1.85em;
     margin-bottom: 15px;
   }
 
