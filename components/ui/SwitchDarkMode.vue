85490635d2576acc4436c611dce2eabf735d9769
Hans Felix <hansfelix50@gmail.com>
2021-08-25 22:34:27 -0500

    fix dark icon

--- components/ui/SwitchDarkMode.vue
+++ components/ui/SwitchDarkMode.vue
@@ -1,41 +1,47 @@
 <template>
   <section class="switch-dark-mode" :class="classSwitchDarkMode">
-    <div class="switch-dark-mode__icon" v-html="rawMoon"></div>
-    <span class="switch-dark-mode__text">{{ $t("ui.darkMode") }}</span>
+    <div class="switch-dark-mode__icon">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path
+          d="M16.01 13.98a5.989 5.989 0 01-5.99-5.99c0-2.711 1.813-4.974 4.283-5.714A10.04 10.04 0 0012 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.478 10-10a9.97 9.97 0 00-.278-2.302c-.738 2.469-3.002 4.282-5.712 4.282z"
+        />
+      </svg>
+    </div>
+    <span class="switch-dark-mode__text">{{ $t('ui.darkMode') }}</span>
     <div class="switch-dark-mode__control" @click="toogleDarkMode">
-      <div class="switch-dark-mode__circle" type="checkbox"></div>
-      <div class="switch-dark-mode__bar" type="checkbox"></div>
+      <div class="switch-dark-mode__circle" type="checkbox" />
+      <div class="switch-dark-mode__bar" type="checkbox" />
     </div>
   </section>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import rawMoon from "~/assets/icons/moon.svg?raw";
+import { mapGetters } from 'vuex'
+import rawMoon from '~/assets/icons/moon.svg?raw'
 
 export default {
-  data() {
-    return { rawMoon };
+  data () {
+    return { rawMoon }
   },
 
   computed: {
     ...mapGetters({
-      isDarkMode: "settings/isDarkMode"
+      isDarkMode: 'settings/isDarkMode'
     }),
 
-    classSwitchDarkMode() {
+    classSwitchDarkMode () {
       return {
-        "switch-dark-mode--active": this.isDarkMode
-      };
+        'switch-dark-mode--active': this.isDarkMode
+      }
     }
   },
 
   methods: {
-    toogleDarkMode() {
-      this.$store.commit("settings/toogleIsDarkMode");
+    toogleDarkMode () {
+      this.$store.commit('settings/toogleIsDarkMode')
     }
   }
-};
+}
 </script>
 
 <style lang="scss">
