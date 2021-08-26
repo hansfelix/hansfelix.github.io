85e03db910d37f0a8143e9386ef1ccf7aba00405
Hans Felix <hansfelix50@gmail.com>
2021-08-25 21:45:55 -0500

    fix html styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,14 +49,8 @@ export default {
 }
 
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+  font-family: Segoe UI, system-ui, -apple-system, sans-serif;
+  font-size: 100%;
 }
 
 *,
