ca7f8540146ffb7b7bd5e4fb7e53fef560158ec5
Hans Felix <hansfelix50@gmail.com>
2021-10-17 20:53:29 -0500

    Move social to bottom

--- components/home-blocks/ContactHomeBlock.vue
+++ components/home-blocks/ContactHomeBlock.vue
@@ -10,6 +10,36 @@
           best to get back to you! We can also be in contact through any of my
           social networks
         </p>
+        <div class="social">
+          <a
+            class="social__item"
+            href="https://www.linkedin.com/in/hans-felix/"
+            target="_blank"
+          >
+            <LinkedinSvg class="icon" />
+          </a>
+          <a
+            class="social__item"
+            href="https://twitter.com/hansfelixramos"
+            target="_blank"
+          >
+            <TwitterSvg class="icon" />
+          </a>
+          <a
+            class="social__item"
+            href="https://stackoverflow.com/users/9176232/hans-felix-ramos?tab=profile"
+            target="_blank"
+          >
+            <StackoverflowSvg class="icon" />
+          </a>
+          <a
+            class="social__item"
+            href="https://github.com/hansfelix"
+            target="_blank"
+          >
+            <GithubSvg class="icon" />
+          </a>
+        </div>
       </div>
     </section>
   </HomeBlockTemplate>
@@ -17,9 +47,20 @@
 
 <script>
 import HomeBlockTemplate from './HomeBlockTemplate.vue'
+// icons imports
+// import BackgroundSvg from '~/assets/icons/background.svg?inline'
+import LinkedinSvg from '~/assets/icons/linkedin.svg?inline'
+import TwitterSvg from '~/assets/icons/twitter.svg?inline'
+import StackoverflowSvg from '~/assets/icons/stackoverflow.svg?inline'
+import GithubSvg from '~/assets/icons/github.svg?inline'
+
 export default {
   components: {
-    HomeBlockTemplate
+    HomeBlockTemplate,
+    LinkedinSvg,
+    TwitterSvg,
+    StackoverflowSvg,
+    GithubSvg
   }
 }
 </script>
@@ -46,6 +87,35 @@ export default {
   &__text {
     font-size: 17px;
     line-height: 1.4;
+  }
+}
+
+.social {
+  display: flex;
+  justify-content: flex-end;
+
+  &__item {
+    width: 40px;
+    height: 40px;
+    margin: 5px;
+    border-radius: 50%;
+    background: transparent;
+    color: inherit;
+    cursor: pointer;
+    transition: 0.2s background ease-in-out;
+    @include flex-and-center;
+
+    &:hover {
+      background-color: #0000001a;
+    }
+  }
+
+  @include mq($until: md) {
+    position: static;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+    padding: 20px 0 0px;
   }
 }
 </style>
