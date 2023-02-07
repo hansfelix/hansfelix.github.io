332afe7dc2878f9fa4c0d41e303dc162b2187266
Hans Felix <hansfelix50@gmail.com>
2023-02-06 23:32:19 -0500

    Add Initial pages

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+
+        <nav class="flex align-center gap-4 p-4">
+          <NuxtLink to="/" class="n-link-base">
+            Home
+          </NuxtLink>
+          <NuxtLink to="/about" class="n-link-base">
+            About
+          </NuxtLink>
+        </nav>
+
+        <div class="text-center p-4 op-50">
+          Current route: <code>{{ route.path }}</code>
+        </div>
   </div>
 </template>
