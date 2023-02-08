09bb0312f8d6dd5877072430e71731f0e1b240a3
Hans Felix <hansfelix50@gmail.com>
2023-02-08 17:46:56 -0500

    remove sample nuxt code

--- app.vue
+++ app.vue
@@ -7,19 +7,6 @@ const route = useRoute()
     <div>
       <NuxtLoadingIndicator />
       <NuxtPage />
-      
-      <nav class="flex align-center gap-4 p-4">
-        <NuxtLink to="/" class="n-link-base">
-          Home
-        </NuxtLink>
-        <NuxtLink to="/about" class="n-link-base">
-          About
-        </NuxtLink>
-      </nav>
-      
-      <div class="text-center p-4 op-50">
-        Current route: <code>{{ route.path }}</code>
-      </div>
     </div>
   </NuxtLayout>
 </template> 
