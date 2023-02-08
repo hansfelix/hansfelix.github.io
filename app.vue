492d7ad90dae2f5c9338a7626493995a916fe8e9
Hans Felix <hansfelix50@gmail.com>
2023-02-07 22:12:17 -0500

    Add template

--- app.vue
+++ app.vue
@@ -3,21 +3,23 @@ const route = useRoute()
 </script>
 
 <template>
-  <div>
-    <NuxtLoadingIndicator />
-    <NuxtPage />
-
-    <nav class="flex align-center gap-4 p-4">
-      <NuxtLink to="/" class="n-link-base">
-        Home
-      </NuxtLink>
-      <NuxtLink to="/about" class="n-link-base">
-        About
-      </NuxtLink>
-    </nav>
-
-    <div class="text-center p-4 op-50">
-      Current route: <code>{{ route.path }}</code>
+  <NuxtLayout>
+    <div>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+      
+      <nav class="flex align-center gap-4 p-4">
+        <NuxtLink to="/" class="n-link-base">
+          Home
+        </NuxtLink>
+        <NuxtLink to="/about" class="n-link-base">
+          About
+        </NuxtLink>
+      </nav>
+      
+      <div class="text-center p-4 op-50">
+        Current route: <code>{{ route.path }}</code>
+      </div>
     </div>
-  </div>
+  </NuxtLayout>
 </template> 
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="default-layout">
+        <slot name="header">
+            Default slot content for <code>Custom</code> layout
+        </slot>
+        <slot />
+    </section>
+</template>
+
+<script lang="ts">
+export default {
+    created() {
+        console.log('layout created')
+    }
+}
+</script>
+
+<style lang="scss">
+.default-layout {
+    max-width: 1320px;
+    margin: auto;
+}
+</style>
